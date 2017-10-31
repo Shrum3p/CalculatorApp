e1fc1b3aa77333bc4bc78934ea360150b2b8287d
Shrum3p <33257238+Shrum3p@users.noreply.github.com>
2017-10-31 09:15:01 -0500

    calculator to update

---
+++ Calculator application Oct. 5th/Calculator application Oct. 5th/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_application_Oct._5th
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            Decimal number2 = 0;
+            Decimal number1 = 0;
+            Console.WriteLine("Give me a number");
+            number1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Give me a number");
+            number2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Enter Operator i.e +");
+            string Symbol = Console.ReadLine();
+          
+            case "+":
+                Console.WriteLine("case 1");
+            break; 
+
+            case ("-";
+            {
+
+            }
+                Console.WriteLine("case 2");
+                break;
+
+            case "*":
+                Console.WriteLine("case 3");
+                break;
+
+            case "/":
+                Console.WriteLine("case 4");
+                break;
+            }
+            }
+
+        }
+    }
+}
